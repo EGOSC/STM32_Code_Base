d8c7fc6e85c3eda7168d04a3d5b81b1ff78d420a
EGOSC <embeddedgallery@gmail.com>
2024-10-21 22:11:38 +0530

    Add files via upload
    Added Base version of IDE Generated version

---
+++ STM32F0/LED_Blink/LED_Blink_IDE/Core/Src/main.c
@@ -0,0 +1,38 @@
+#define GPIOA_BASE 	(unsigned int *)0x48000000
+#define GPIOA_ODR 	(unsigned int *)0x48000014
+#define RCC_BASE	(unsigned int *)0x40021000
+#define RCC_AHBENR	(unsigned int *)0x40021014
+#define	GPIOAEN		17
+#define ODR			0x14
+#define MODER 		0
+int Pin =5;
+int MODER_Size =2;
+int ODR_Size = 1;
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  *RCC_AHBENR	|= 0x1 << GPIOAEN;
+  *(GPIOA_BASE) |= 0x1 << (Pin*MODER_Size);
+  *(GPIOA_ODR) |= 0x1 << (Pin*ODR_Size);
+
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+	  *(GPIOA_ODR) |= 0x1 << (Pin*ODR_Size);
+	  for(int i=0; i< 100000;)
+	  {
+		  i++;
+	  }
+	  *(GPIOA_ODR) &= ~(0x1 << (Pin*ODR_Size));
+	  for(int i=0; i< 100000;)
+	  {
+		  i++;
+	  }
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
